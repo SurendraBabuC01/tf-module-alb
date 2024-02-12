2db002613daf97547f10162cc2c97a50a95ea104
surendra Babu C <surendra9703802865@gmail.com>
2024-02-12 18:59:13 +0530

    terraform

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "main" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "ALB"
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
